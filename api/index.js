217ed63e79810cfc0574643cabab77ef231c8d4a
dheerajhemachandran <dheerajhemachandran@gmail.com>
2023-04-10 07:21:03 +0530

    node deploy

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import postRouter from "./route/post.js"
 const app = express();
 app.use(express.json())
 app.use(cors());
-const port = 5000;
+const port = 4000;
 
 app.use("/auth",authRouter)
 app.use("/post",postRouter)
